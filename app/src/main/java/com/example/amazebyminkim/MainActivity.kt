5c4d34392afa2f2e00507f9e29ed5a725fc3ee17
Min Kim <mkim22@email.wm.edu>
2022-11-13 17:28:52 -0500

    second commit

--- app/src/main/java/com/example/amazebyminkim/MainActivity.kt
+++ app/src/main/java/com/example/amazebyminkim/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello0 $name!")
 }
 
 @Preview(showBackground = true)
