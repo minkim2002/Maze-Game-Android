31711b8c40db94c329095562875adba36b3f6dbb
Min Kim <mkim22@email.wm.edu>
2022-11-20 01:39:16 -0500

    GeneratingActivity implemented

--- app/src/main/java/edu/wm/cs/cs301/MinKim/gui/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/MinKim/gui/AMazeActivity.java
@@ -1,29 +1,118 @@
 
 package edu.wm.cs.cs301.MinKim.gui;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+import java.util.Objects;
 
 import edu.wm.cs.cs301.MinKim.R;
 
+/**
+ * @author Min Kim
+ * Class: AMazeActivity
+ * Responsibilities: Serves as a main screen for the maze game, select a skill level, choose a builder,
+ * choose whether there will be rooms or not, and connect other screens
+ * Collaborators: GeneratingActivity, PlayManuallyActivity, PlayAnimationActivity
+ */
 public class AMazeActivity extends AppCompatActivity {
 
+    ActivityResultLauncher<Intent> startForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Bundle maze = result.getData().getExtras();
+
+                assert(maze != null) : "Error: maze configuration is not supposed to be null";
+
+                String driverString = Objects.requireNonNull(maze.get("Driver")).toString();
+                String robotString = Objects.requireNonNull(maze.get("Robot")).toString();
+                Log.v("Driver Chosen", driverString);
+                Log.v("Robot Chosen", robotString);
+
+                Intent game = new Intent(AMazeActivity.this,
+                        (driverString.equals("Manual") ? PlayManuallyActivity.class : PlayAnimationActivity.class));
+                game.putExtra("Maze", "");
+                game.putExtra("Driver", driverString);
+                game.putExtra("Robot", robotString);
+
+                Toast toast = Toast.makeText(AMazeActivity.this, "Loading game", Toast.LENGTH_SHORT);
+                toast.show();
+                startActivity(game);
+            }
+            else if (result.getResultCode() == Activity.RESULT_CANCELED) {
+                Log.w("Generation", "Canceled");
+            }
+        }
+    });
+
+    /**
+     * Start the background animation, make the builder
+     * spinner with various builder options, set up the navigation buttons
+     * @param savedInstanceState a reference to the bundle object
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.title);
+        Log.v("Launching Maze App", "Passed");
 
-        Spinner builderSpinner = findViewById(R.id.builderSpinner);
+        Spinner builder = findViewById(R.id.builderSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.builder, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        builderSpinner.setAdapter(adapter);
+        builder.setAdapter(adapter);
 
-        Spinner driverSpinner = findViewById(R.id.driverSpinner);
-        adapter = ArrayAdapter.createFromResource(this, R.array.driver, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        driverSpinner.setAdapter(adapter);
+        ActivityResultLauncher<Intent> getResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // There are no request codes
+                        Intent data = result.getData();
+                    }
+                });
+
+        setUpNavigationButton((Button) findViewById(R.id.revisitButton));
+        setUpNavigationButton((Button) findViewById(R.id.exploreButton));
+    }
+
+    public void setUpNavigationButton(Button navigationButton) {
+        navigationButton.setOnClickListener(view -> {
+
+            Spinner builder = findViewById(R.id.builderSpinner);
+            String builderString = builder.getSelectedItem().toString();
+
+            SeekBar skillLevel = findViewById(R.id.seekBar);
+            int lv = skillLevel.getProgress();
+
+            SwitchMaterial room = findViewById(R.id.roomSwitch);
+            boolean isRoom = room.isChecked();
+
+
+            Intent generation = new Intent(AMazeActivity.this, GeneratingActivity.class);
+            generation.putExtra("Builder", builderString);
+            generation.putExtra("SkillLevel", lv);
+            generation.putExtra("Room", isRoom);
+
+            // make a new toast to alert the user of the new activity, start the new activity
+            // and await the result
+            Toast toast = Toast.makeText(AMazeActivity.this, "Generating maze", Toast.LENGTH_SHORT);
+            toast.show();
+            startForResult.launch(generation);
+        });
     }
 }
--- app/src/main/java/edu/wm/cs/cs301/MinKim/gui/GeneratingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/MinKim/gui/GeneratingActivity.java
@@ -1,4 +1,115 @@
 package edu.wm.cs.cs301.MinKim.gui;
 
-public class GeneratingActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.wm.cs.cs301.MinKim.R;
+
+public class GeneratingActivity extends AppCompatActivity implements Runnable {
+
+    protected Thread generatingThread;
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.generating);
+
+        generatingThread = new Thread(this);
+        generatingThread.start();
+        handler = new Handler(Looper.myLooper());
+
+        Spinner robot = findViewById(R.id.robotSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.robot, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        robot.setAdapter(adapter);
+
+        Spinner driver = findViewById(R.id.driverSpinner);
+        adapter = ArrayAdapter.createFromResource(this, R.array.driver, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        driver.setAdapter(adapter);
+
+        driver.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                LinearLayout robotLayout = findViewById(R.id.robotLayout);
+                if(!parent.getItemAtPosition(position).toString().equals("Wallfollower") &&
+                        !parent.getItemAtPosition(position).toString().equals("SmarterWallfollower") ) robotLayout.setVisibility(View.INVISIBLE);
+                else robotLayout.setVisibility(View.VISIBLE);
+            }
+            public void onNothingSelected(AdapterView<?> parent) {
+                LinearLayout robotLayout = findViewById(R.id.robotLayout);
+                if(!parent.getSelectedItem().toString().equals("Wallfollower") &&
+                        !parent.getSelectedItem().toString().equals("SmarterWallfollower") ) robotLayout.setVisibility(View.INVISIBLE);
+                else robotLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        Button play = findViewById(R.id.playButton);
+        play.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                Spinner driver = findViewById(R.id.driverSpinner);
+                String driverString = driver.getSelectedItem().toString();
+
+                Spinner robot = findViewById(R.id.robotSpinner);
+                String robotString = robot.getSelectedItem().toString().split(" ")[0];
+
+                Intent result = new Intent(GeneratingActivity.this, AMazeActivity.class);
+                result.putExtra("Driver", driverString);
+                result.putExtra("Robot", robotString);
+
+                GeneratingActivity.this.setResult(RESULT_OK, result);
+                GeneratingActivity.this.finish();
+            }
+        });
+    }
+
+    @Override
+    public void run() {
+        Intent intent = getIntent();
+        Log.v("SkillLevel", ""+intent.getIntExtra("SkillLevel", 0));
+        Log.v("Builder", ""+intent.getStringExtra("Builder"));
+        Log.v("Room", ""+intent.getBooleanExtra("Room", false));
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        int currentProgress = 0;
+        while (currentProgress <= 100) {
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                setResult(RESULT_CANCELED, null);
+                finish();
+                return;
+            }
+            currentProgress += 10;
+            progressBar.setProgress(currentProgress);
+        }
+
+        handler.post(new Runnable() {
+            public void run() {
+                Button playButton = findViewById(R.id.playButton);
+                playButton.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        generatingThread.interrupt();
+        Log.v("Generation", "Cancelling generation process");
+        GeneratingActivity.this.setResult(RESULT_CANCELED, new Intent(GeneratingActivity.this, AMazeActivity.class));
+        GeneratingActivity.this.finish();
+    }
 }
