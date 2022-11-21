cd0bfcef96591f74c3750c737c638da60b3aa9bd
Min Kim <mkim22@email.wm.edu>
2022-11-21 15:05:01 -0500

    GeneratingActivity fully implemented with comments for better readability

--- app/src/main/java/edu/wm/cs/cs301/MinKim/gui/GeneratingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/MinKim/gui/GeneratingActivity.java
@@ -16,76 +16,103 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import edu.wm.cs.cs301.MinKim.R;
 
+/**
+ * @author Min Kim
+ * Class: GeneratingActivity
+ * Responsibilities: Screen during the maze generating process, Selecting a driver
+ * and robot, Sending information back to AMazeActivity.
+ * Collaborators: AMazeActivity
+ */
+
 public class GeneratingActivity extends AppCompatActivity implements Runnable {
 
-    protected Thread generatingThread;
+    protected Thread generating;
     private Handler handler;
 
+    /**
+     * Start the maze generation process thread,
+     * make the driver and robot spinners with their corresponding options,
+     * set up listeners for the driver spinner (for robot selecting process) and play button
+     * @param savedInstanceState a reference to the bundle object
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.generating);
 
+        // start the thread for generation
         handler = new Handler(Looper.getMainLooper());
-        generatingThread = new Thread(this);
-        generatingThread.start();
+        generating = new Thread(this);
+        generating.start();
 
+        // Robot spinner with robot options
         Spinner robot = findViewById(R.id.robotSpinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.robot, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         robot.setAdapter(adapter);
 
+        // Driver spinner with driver options
         Spinner driver = findViewById(R.id.driverSpinner);
         adapter = ArrayAdapter.createFromResource(this, R.array.driver, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         driver.setAdapter(adapter);
 
+        // Listener for the driver spinner for different robot layout
         driver.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                LinearLayout robotLayout = findViewById(R.id.robotLayout);
+                LinearLayout robot = findViewById(R.id.robotLayout);
                 if(!parent.getItemAtPosition(position).toString().equals("Wall Follower") &&
                         !parent.getItemAtPosition(position).toString().equals("Wizard"))
-                    robotLayout.setVisibility(View.INVISIBLE);
-                else robotLayout.setVisibility(View.VISIBLE);
+                    robot.setVisibility(View.INVISIBLE);
+                else robot.setVisibility(View.VISIBLE);
             }
             public void onNothingSelected(AdapterView<?> parent) {
-                LinearLayout robotLayout = findViewById(R.id.robotLayout);
+                LinearLayout robot = findViewById(R.id.robotLayout);
                 if(!parent.getSelectedItem().toString().equals("Wall Follower") &&
                         !parent.getSelectedItem().toString().equals("Wizard"))
-                    robotLayout.setVisibility(View.INVISIBLE);
-                else robotLayout.setVisibility(View.VISIBLE);
+                    robot.setVisibility(View.INVISIBLE);
+                else robot.setVisibility(View.VISIBLE);
             }
         });
 
+        //listener for the play button
         Button play = findViewById(R.id.playButton);
         play.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-
-                Spinner driver = findViewById(R.id.driverSpinner);
-                String driverString = driver.getSelectedItem().toString();
-
+                //robot information
                 Spinner robot = findViewById(R.id.robotSpinner);
                 String robotString = robot.getSelectedItem().toString().split(" ")[0];
 
+                //driver information
+                Spinner driver = findViewById(R.id.driverSpinner);
+                String driverString = driver.getSelectedItem().toString();
+
+                //Intent with robot and driver information
                 Intent result = new Intent(GeneratingActivity.this, AMazeActivity.class);
                 result.putExtra("Driver", driverString);
                 result.putExtra("Robot", robotString);
 
+                // send the result back to the AMazeActivity
                 GeneratingActivity.this.setResult(RESULT_OK, result);
                 GeneratingActivity.this.finish();
             }
         });
     }
 
+    /**
+     * Thread process during the maze generation
+     */
     @Override
     public void run() {
+        //Get the intent sent from the AMazeActivity
         Intent intent = getIntent();
-        Log.v("SkillLevel", ""+intent.getIntExtra("SkillLevel", 0));
+        Log.v("Difficulty Level", ""+intent.getIntExtra("Difficulty Level", 0));
         Log.v("Builder", ""+intent.getStringExtra("Builder"));
-        Log.v("Room", ""+intent.getBooleanExtra("Room", false));
+        Log.v("Room", ""+intent.getBooleanExtra("Room", true));
 
-        int currentProgress = 0;
-        while (currentProgress < 100) {
+        // Progress
+        int progress = 0;
+        while (progress < 100) {
             try {
                 Thread.sleep(1000);
             } catch (Exception e) {
@@ -93,22 +120,28 @@ public class GeneratingActivity extends AppCompatActivity implements Runnable {
                 finish();
                 return;
             }
-            currentProgress += 10;
+            progress += 10;
         }
 
+        //once the progress is done, play button becomes visible
         handler.post(new Runnable() {
             public void run() {
-                Button playButton = findViewById(R.id.playButton);
-                playButton.setVisibility(View.VISIBLE);
+                Button play = findViewById(R.id.playButton);
+                play.setVisibility(View.VISIBLE);
             }
         });
     }
 
+    /**
+     * If the player presses the back button, the generating thread is interrupted and the title
+     * activity is notified.
+     */
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        generatingThread.interrupt();
-        Log.v("Generation", "Cancelling generation process");
+        // stop the thread
+        generating.interrupt();
+        Log.v("Generation", "generation process interrupted");
         GeneratingActivity.this.setResult(RESULT_CANCELED, new Intent(GeneratingActivity.this, AMazeActivity.class));
         GeneratingActivity.this.finish();
     }
