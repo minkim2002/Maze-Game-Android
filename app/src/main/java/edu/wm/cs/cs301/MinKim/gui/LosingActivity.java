45597266be2c9244326d49b19ff2c825d1de4a90
Min Kim <mkim22@email.wm.edu>
2022-11-21 11:02:52 -0500

    LosingActivity implemented

--- app/src/main/java/edu/wm/cs/cs301/MinKim/gui/LosingActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/MinKim/gui/LosingActivity.java
@@ -1,1 +1,15 @@
+package edu.wm.cs.cs301.MinKim.gui;
 
+import android.os.Bundle;
+
+import edu.wm.cs.cs301.MinKim.R;
+
+public class LosingActivity extends FinalActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.losing);
+        setUpComponents(this, false);
+    }
+}
